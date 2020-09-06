7c336ce5fb6f0688d24bbf83e9d09601bf3ca2b6
SamukaAlves000 <60469274+SamukaAlves000@users.noreply.github.com>
2020-09-06 18:52:37 -0300

    Exemplo de validação de formulário
    Exemplo de validação de formulário

--- barbearia/src/main/java/com/samuel/barbearia/BarbeariaApplication.java
+++ barbearia/src/main/java/com/samuel/barbearia/BarbeariaApplication.java
@@ -1,4 +1,4 @@
-package com.samuel.barbearia;
+ package com.samuel.barbearia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
